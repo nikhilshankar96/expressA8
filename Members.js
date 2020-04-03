9f7156d6a21038f7e7b65a4a1872d76bc17a7887
Nikhil Shankar <nikhil_shankar@outlook.com>
2020-04-02 22:39:43 -0400

    basic stuff

---
+++ Members.js
@@ -0,0 +1,19 @@
+const members = [
+	{
+		id: "nps@abc.com",
+		email: "nps@abc.com",
+		password: "Abcd1234!",
+		products: [
+			{
+				name: "prod1",
+				price: "40"
+			},
+			{
+				name: "propd2",
+				price: "30"
+			}
+		]
+	}
+];
+
+module.exports = members;
---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+const members = require("./Members");
+const moment = require("moment");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+//middleware
+const logger = (req, res, next) => {
+	console.log("Logger function");
+	console.log(
+		`${req.protocol}://${req.get("host")}${
+			req.originalUrl
+		}: ${moment().format()}`
+	);
+
+	next();
+};
+
+//init middleware
+app.use(logger);
+
+//routes
+
+//all members route
+app.get("/api/members", (req, res) => {
+	res.json(members);
+});
+
+//set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+	console.log(`Server started on port: ${PORT}`);
+});
